38acb6609f71c8d4cda6570fe32b3ffbc00b10b2
Morgan <morgandallred123@gmail.com>
2015-01-22 11:25:46 -0700

    Final Copy

--- Project_02/Driver.cpp
+++ Project_02/Driver.cpp
@@ -16,9 +16,9 @@ int main()
 	vector<Account> accVector;
 
 	// Create three person objects.You can use the names and addresses shown in the example output, or use your imagination to come up with something interesting.
-	Person p1("Margan A.", "1234 Street");
-	Person p2("Wes C.", "4321 Cir.");
-	Person p3("Homer Simpson", "5748 Springfield");
+	Person p1("Morgan A.", "1234 Street");
+	Person p2("Westley C.", "4321 Cir.");
+	Person p3("Homer Simpson", "5748 Spring");
 	// Create three Account objects, where each account object contains one of your Person objects.Initialize the balance in the first account object to $500.00, the balance in the second to $1000.00, and the balance in the third to $1200.00.
 	Account a1(p1, 500.00, 1);
 	Account a2(p2, 1000.00, 2);
@@ -41,6 +41,7 @@ int main()
 	// Finally, pass the vector to your displayAccounts function to display the account information for each Account object in th*/e vector.
 	displayAccounts(accVector);
 
+	cout << endl;
 	system("Pause");
 	return 0;
 
@@ -50,10 +51,12 @@ void displayAccounts(const vector<Account>& _aVector)
 {
 	cout << "National Bank\n" << endl;
 	cout << "Account\tAccount" << endl;
-	cout << "Number\tName\tAddress\tBalance" << endl;
+	cout << "Number\tName\t\t\tAddress\t\t\tBalance\n" << endl;
 	for (unsigned i = 0; i < _aVector.size(); i++)
 	{
-		cout << _aVector[i].getAccountNumber() << "\t" << _aVector[i].getPerson().getName() << "\t" << _aVector[i].getPerson().getAddress() << "\t" << _aVector[i].getAccountBalance() << endl;
+		cout.setf(ios::fixed);
+		cout.precision(2);
+		cout << _aVector[i].getAccountNumber() << "\t" << _aVector[i].getPerson().getName() << "\t\t" << _aVector[i].getPerson().getAddress() << "\t\t$" << _aVector[i].getAccountBalance() << endl;
 	}
 
 }
